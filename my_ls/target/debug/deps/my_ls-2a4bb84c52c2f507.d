c6ee77cd87bfe624b52583c84965dc87b3b66e7d
LadyBobb <134429069+LadyBobb@users.noreply.github.com>
2024-02-17 17:04:19 -0300

    First commit

---
+++ my_ls/target/debug/deps/my_ls-2a4bb84c52c2f507.d
@@ -0,0 +1,5 @@
+/Users/glllcs/Documents/repos/rustalchemy/my_ls/target/debug/deps/libmy_ls-2a4bb84c52c2f507.rmeta: src/main.rs
+
+/Users/glllcs/Documents/repos/rustalchemy/my_ls/target/debug/deps/my_ls-2a4bb84c52c2f507.d: src/main.rs
+
+src/main.rs:
---
+++ my_ls/target/debug/deps/my_ls-81bef4d5dcb319e6.d
@@ -0,0 +1,5 @@
+/Users/glllcs/Documents/repos/rustalchemy/my_ls/target/debug/deps/libmy_ls-81bef4d5dcb319e6.rmeta: src/main.rs
+
+/Users/glllcs/Documents/repos/rustalchemy/my_ls/target/debug/deps/my_ls-81bef4d5dcb319e6.d: src/main.rs
+
+src/main.rs:
